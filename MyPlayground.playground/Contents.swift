ae110cdd80ade846f8a50a49535cb37bf7f8fdbd
PrasannaKumarDev <valkiprassannakumar@gmail.com>
2026-08-23 11:14:23 +0530

    swift practice

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,66 +1,62 @@
-import UIKit
+let name : String = "prasanna Kumar"
+var age : Int = 29
+var height : Double = 5.8
+var iosDev : Bool = true
+let intro = "Myself  \(name),\(age) years old.with \(height) tall, aspirant IOS Developer \(iosDev)"
 
-var firstName = "Prasanna"
-var lastName = "Kumar"
-var age = 28
-var height = 5.8
-var isIosDev = true
-var  pi = 3.14
-var fullName = firstName + lastName
-var info = "I am \(fullName),\(age) years old,with \(height),working as flutter developer \(isIosDev)"
-var heightInfo = "my heifght is " + String(height)
-let constant : Int = 20
-var number : Int = 30
-var totalNumber = constant + number
-var surname : String?
-surname = "Ios Developer"
-if surname == nil{
-    var name = "my surname" + surname!
+
+
+func add(_ a : Int, to b : Int) -> Int {
+    return a + b;
 }
-var upperOne = firstName.uppercased()
-var lowerOne = upperOne.lowercased()
+let result = add(5,to: 15)
 
-let humanAge = 17
 
-if humanAge >= 18{
-    print("Eligible to vote!")
-   
+var optionalString : String? = "Prasanna kumar"
+optionalString = nil
+var numbers = [10,20,30,40]
+numbers.append(50)
+let first = numbers[0]
+print(numbers)
+print(first)
+
+for number in numbers {
+    print("numbers : \(number)")
+}
+
+var user : [String: Any] = ["name":"prasanna kumar","age":29,"role": "Ios Developer"]
+print(user)
+
+var uniqueNumbers : Set = [1,2,3]
+print(uniqueNumbers)
+
+
+var ages = 12
+if ages >= 18 {
+    print("eligible to vote")
 } else {
-    print("not eligible to vote!")
+    print("not eligible")
 }
 
-
-switch humanAge {
-case age...:
-    print("Eligible to vote!")
+switch ages {
+case 0..<13:
+    print("Child")
+case 13..<18:
+    print("Teen")
+case 18...:
+    print("adult")
 default:
-    print("Not eligible to vote!")
+    print("unknown")
 }
 
-var names = ["Prasanna","Akshaya","Himavarshini"]
-names[0]
-names.append("Ammulu")
-print(names)
 
-var bookOfCollections = ["Money","cash"]
-bookOfCollections[0]
-bookOfCollections.append("Card")
-bookOfCollections.count
-for book in bookOfCollections{
-    print(bookOfCollections)
-}
-var bookCollectionDict = ["1328683788": "Tool of Titans", "0307463745": "Rework", "1612060919": "Authority"]
-bookCollectionDict["0307463745"]
-for (key, value) in bookCollectionDict {
-print("ISBN: \(key)")
-print("Title: \(value)")
-}
+print(result)
+print(intro)
 
-var jobTitle : String?
-jobTitle = "Ios Developer"
-if jobTitle == nil{
-    var message = "I am looking for" + jobTitle!
+var i = 0
+while i < 5 {
+    print(i)
+    i += 1
 }
-
 
 
